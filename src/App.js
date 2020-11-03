a6b6a343be79fdac666f1849c38f13a49b026303
Philippe Roemer <philipperoemer@yahoo.com>
2020-11-02 19:14:07 -0500

    Update App.js
    ATH integration

--- src/App.js
+++ src/App.js
@@ -2,17 +2,14 @@ import React, { useState, useEffect, useContext } from 'react';
 import axios from 'axios';
 import Logo from './CC_Logo.png';
 import './App.css';
-//import Coin from './Coin';
 
 function App() {
   const [coins, setCoins] = useState([]);
-  //const [search, setSearch] = useState('');
 
   //Axios fetch data from API
   useEffect(() => {
     axios
       .get(
-        //IOTA API Call -->'https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&ids=iota&order=market_cap_desc&per_page=1&page=1&sparkline=false'
         'https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=50&page=1&sparkline=false'
       )
       .then(res => {
@@ -22,17 +19,29 @@ function App() {
       .catch(error => console.log(error));
   }, []);
 
+//This is currently a mess, but it will be cleaned up soon
+
 function  CurrentValue(){
-  //console.log('Button Clicked')
   const amount = document.getElementById("amount").value;
   const crypto = document.getElementById("crypto").value;
 
   const x = document.getElementById("crypto").selectedIndex;
   const y = document.getElementById("crypto").options;
+  console.log(y[x].dataset.ath);
   console.log(y[x]);
-  const value = amount * crypto;  
 
-  document.getElementById("currentValue").innerHTML = "The current value of " + amount + " " + y[x].text + " is : <span class='totalAmount'>$" + value.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,') + "</span>";
+  const ath_Value = y[x].dataset.ath * 1;
+  const ath_TotalValue = y[x].dataset.ath * amount;
+  const ath_Date = y[x].dataset.athdate;
+
+  var d = new Date(ath_Date);
+  var n = d.toLocaleDateString('en-US');
+
+  const user_value = amount * crypto;  
+
+  document.getElementById("currentValue").innerHTML = "The current value of " + amount + " " + y[x].text + " is  <span class='totalAmount'>$" + user_value.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,') + "</span>";
+  document.getElementById("athValue").innerHTML = "On " + n + ", the all-time-high (ATH) value for 1 " + y[x].text + " was <span class='totalAmount'>$" + ath_Value.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,') + "</span>";
+  document.getElementById("athValue2").innerHTML = "The value of " + amount + " " + y[x].text + " during its ATH would've been worth <span class='totalAmount'>$" + ath_TotalValue.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,') + "</span>";
 }
 
   return (
@@ -49,15 +58,17 @@ function  CurrentValue(){
         <p>Select a cryptocurrency</p>
         <select id="crypto" class="cryptoCurrency">
           {coins.map(post => (
-            <option value={post.current_price}>{post.name}</option>
+            <option value={post.current_price} data-ath={post.ath} data-athdate={post.ath_date} >{post.name}</option>
           ))}                        
         </select>
       </div>
     </div>
 
-    <input type="button" onClick={CurrentValue} value="Submit" class="submitButton"/>
+  <input type="button" onClick={CurrentValue} value="Submit" class="submitButton"/>
   
   <p id="currentValue" class="valueText"></p>
+  <p id="athValue" class="valueText"></p>
+  <p id="athValue2" class="valueText"></p>
   
 </div>
   );
